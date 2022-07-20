971fe51d72def1f9874d2f8bc72a6ce99810a11b
irao12 <ivanrao@outlook.com>
2022-07-19 22:15:57 -0400

    added the contact page

---
+++ src/contact.js
@@ -0,0 +1,39 @@
+function initContact () {
+    const main = document.querySelector('.main');
+    main.textContent = '';
+    
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+
+    const contactHeading = document.createElement('h1');
+    contactHeading.textContent = 'Contact Us';
+
+    const phoneHeading = document.createElement('h2');
+    phoneHeading.textContent = 'Phone Number';
+    phoneHeading.classList.add('phone-heading');
+
+    const phoneNum = document.createElement('p');
+    phoneNum.classList.add('phone-number');
+    phoneNum.textContent = '23123-2323-23232';
+
+    const addressHeading = document.createElement('h2');
+    addressHeading.classList.add('address-heading');
+    addressHeading.textContent = 'Address';
+    const address = document.createElement('p');
+    address.classList.add('address');
+    address.textContent = 'Somewhere out there';
+
+    const restaurantImage = document.createElement('img');
+    restaurantImage.src = '../dist/images/restaurant.jpg';
+
+    contact.appendChild(contactHeading);
+    contact.appendChild(phoneHeading);
+    contact.appendChild(phoneNum);
+    contact.appendChild(addressHeading);
+    contact.appendChild(address);
+    contact.appendChild(restaurantImage);
+
+    main.appendChild(contact);
+}
+
+export default initContact;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import initWebsite from './website.js';
 import initMenu from './menu.js'
 import initHome from './home.js';
+import initContact from './contact.js'
 
 initWebsite();
 
 document.querySelector('.home-button').addEventListener('click', initHome);
 document.querySelector('.menu-button').addEventListener('click', initMenu);
+document.querySelector('.contact-button').addEventListener('click', initContact);
